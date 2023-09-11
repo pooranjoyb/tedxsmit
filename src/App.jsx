4eed8b0f0e7d49e93eecde8584f36114ab598c04
pooranjoyb <pooranjoyb2016@gmail.com>
2023-09-12 00:47:21 +0530

    Added tailwind and bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Home from "./Components/Home"
+import Screen from "./Components/Screen"
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <div className="background">
+        <Screen />
+        <div className="z-5 ml-[30%]">
+          <img src="./assets/logo-white.png" width={1200} />
+        </div>
+      </div>
     </>
   )
 }
