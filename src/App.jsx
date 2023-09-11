06fb872ba88ad9aa180dcc2f93d8bc824d4d7f67
pooranjoyb <pooranjoyb2016@gmail.com>
2023-09-12 00:56:17 +0530

    SCreen v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Screen from "./Components/Screen"
-
+import Home from "./Components/Home"
 function App() {
 
   return (
     <>
       <div className="background">
         <Screen />
-        <div className="z-5 ml-[30%]">
-          <img src="./assets/logo-white.png" width={1200} />
-        </div>
+        <Home/>
       </div>
     </>
   )
---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="ml-[25%] z-20 text-center w-1/2">
+            <img src="./assets/logo-white.png" width={800} />
+            TEDxSMIT is an annual event that aims to bring together inquisitive minds and spark engaging conversations. With a confluence of thought provoking speakers, we hope to bring forward fresh perspectives and novel concepts, giving every participant something to take home.
+        </div>
+    )
+}
