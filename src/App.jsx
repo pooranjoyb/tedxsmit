46f27526c1ef294b2d6a2a446722b5016a4df42f
pooranjoyb <pooranjoyb2016@gmail.com>
2023-09-12 02:21:36 +0530

    Added Gallery v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import Screen from "./Components/Screen";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About"
+import Gallery from './Components/Gallery'
+
 function App() {
   return (
     <>
       <Navbar />
       <Screen />
+      <Home />
       <div className="content">
-        <Home />
         <About />
+        <div className="flex w-full items-center justify-center my-[3rem]">
+          <Gallery />
+        </div>
       </div>
     </>
   );
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <>
-    <div className='ml-[33%] z-20 text-center w-1/2 font-bold text-xl text-red-900'>
-    <img className='' src="./assets/rising.png" width={500} />
-    </div>
-    
-    </>
-  )
+    return (
+        <>
+            <div className='flex-auto flex-col items-center justify-center w-full z-20 text-center w-3/5'>
+                <img className='inline-block' src="./assets/rising.png" width={500} />
+                <div className='mx-[12rem] my-5 text-center font-sm'>
+                    Centered around the theme "Rising Above Ranges" for the 6th Edition of TEDxSMIT 2023, our objective is to cultivate a
+                    mindset that transcends the barriers obstructing progress. Through this, we aspire to navigate towards a future that is not
+                    only more radiant and expansive but also inherently superior.
+            </div>
+            </div>
+        </>
+    )
 }
